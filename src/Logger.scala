f88e24db8fb5fd36519e2cd772a0b1cf7a0caf33
Hannes Metssalu <hannesss81@gmail.com>
2014-12-29 13:38:09 +0200

    Testing

--- src/Logger.scala
+++ src/Logger.scala
@@ -5,8 +5,10 @@ object Logger {
     println("\nSheathe your sword, draw your deck, and get ready for Hearthstone - the fast \npaced strategy card game that's easy to learn and massively fun.")
   }
 
-  // TODO mocked
-  def askCardsFile(player: String): String = ""
+  def askCardsFile(player: String): String = {
+    println("\nPlease enter the card file name for " + player + ".")
+    scala.io.StdIn.readLine("Filename: ")
+  }
 
   def sayGameResultsAndEndIt(): Unit = {
     println("\nGame is over!")
